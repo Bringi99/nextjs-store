75055f991a2846b78272e6bf24e7dd6aa5e55362
amin <amin@mail.com>
2025-01-29 22:07:48 +0300

    create-website-layout

---
+++ lib/data.ts
@@ -0,0 +1,40 @@
+
+const data = {
+    headerMenus:[
+        {
+            name:"Todays Deal",
+            href:'/search?tag=todays-deal',
+        },
+        {
+            name:"New Arrivals",
+            href:'/search?tag=new-arrivals',
+        },
+        {
+            name:"Featured Products",
+            href:'/search?tag=featured',
+        },
+        {
+            name:"Best Sellers",
+            href:'/search?tag=best-sellers',
+        },
+        {
+            name:"Browsing History",
+            href:'/#browsing-history',
+        },
+        {
+            name:"Customer Service",
+            href:"page/customer-service",
+        },
+        {
+            name:'About Us',
+            href:"page/about-us",
+        },
+        {
+            name:'Help',
+            href:'/help',
+        },
+        
+        
+    ],
+}
+export default data
